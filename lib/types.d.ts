757e696563bbf60152a5111797a07df4f505ad68
Rithvik Nishad <rithvikn2001@gmail.com>
2022-10-11 09:39:01 +0530

    add example definition

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,10 +1,66 @@
 export declare type ProsumerID = string;
+
 export declare type Power = number;
+
 export declare type Price = number;
-export declare type ProsumerClass = "SOLAR" | "WIND" | "LIMIT";
-export declare type ProsumerSnapshotData = {
-  exportPower: Power;
-  baseSellingPrice: Price;
-  class: ProsumerClass;
+
+export declare type GenerationClass = "SOLAR" | "WIND" | "LIMIT";
+
+export declare type DemandClass =
+  | "RESEDENTIAL"
+  | "GENERAL"
+  | "LIMIT"
+  | "EV"
+  | "BATTERY"
+  | "FARM";
+
+export declare type ProsumerSnapshot<T extends GenerationClass | DemandClass> =
+  {
+    power: Power;
+    class: T;
+  };
+export declare type ClassBaseSellingPrices = Record<GenerationClass, Price>;
+
+/**
+ * Snapshot contains three main lookup tables:
+ * * `classPrices`
+ * * `sellers`
+ * * `buyers`
+ *
+ * ### Additional Notes:
+ *
+ *  * "LIMIT" class means it's an external source such as grid, and hence
+ *    market price evaluation service knows that this price is not flexible.
+ *
+ *  * The difference between the sum of all the power of sellers and sum of all
+ *    power of buyers gives the power loss in the grid. This loss shall not
+ *    exceed beyond 10% of the total generation for any scenario, as it is
+ *    an unlikely ssituation.
+ */
+export declare type Snapshot = {
+  /**
+   * #### Class Prices:
+   *    This lookup table shall contain all the generation class specific base
+   *    selling price. Keys are the generation class names, and values are the
+   *    associated selling price of the key.
+   */
+  classPrices: ClassBaseSellingPrices;
+
+  /**
+   * #### Buyers:
+   *    This lookup table shall contain all the sellers data. Key is the prosumer
+   *    ID, and value contains record of `power` and `class`.
+   *    - `power` shall be in kilowatts.
+   *    - `class` shall be one of the following: "RESEDENTIAL", "GENERAL", "LIMIT"
+   */
+  buyers: Record<ProsumerID, ProsumerSnapshot<DemandClass>>;
+
+  /**
+   * #### Sellers:
+   *    This lookup table shall contain all the sellers data. Key is the prosumer
+   *    ID, and value contains record of `power` and `class`.
+   *    - `power` shall be in kilowatts.
+   *    - `class` shall be one of the keys in the `classPrices` lookup table.
+   */
+  sellers: Record<ProsumerID, ProsumerSnapshot<GenerationClass>>;
 };
-export declare type Snapshot = Record<ProsumerID, ProsumerSnapshotData>;
--- snapshots/snapshot1.ts
+++ snapshots/snapshot1.ts
@@ -1,10 +1,26 @@
 import { Snapshot } from "../lib/types";
 
 const snapshot: Snapshot = {
-  "0001": {
-    exportPower: 1000,
-    baseSellingPrice: 7,
-    class: "LIMIT",
+  classPrices: {
+    LIMIT: 7.0,
+    SOLAR: 3.5,
+    WIND: 4.2,
+  },
+
+  sellers: {
+    "000": { power: 12000, class: "LIMIT" },
+    "001": { power: 2000, class: "SOLAR" },
+    "002": { power: 2000, class: "SOLAR" },
+    "003": { power: 3000, class: "WIND" },
+    "004": { power: 2000, class: "WIND" },
+  },
+
+  buyers: {
+    "005": { power: 10000, class: "GENERAL" },
+    "006": { power: 1900, class: "GENERAL" },
+    "007": { power: 7000, class: "GENERAL" },
+    "008": { power: 1000, class: "GENERAL" },
+    "009": { power: 0, class: "LIMIT" },
   },
 };
 
